72189f18997f799ebeefafda1647f24ec50b7451
Gyl <yalonggong@gmail.com>
2024-07-11 17:01:41 +0800

    add tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import WebApp from "@twa-dev/sdk";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl="https://gyloong.github.io/ton-test/manifest.json">
+      <App />
+    </TonConnectUIProvider>
   </React.StrictMode>
 );
